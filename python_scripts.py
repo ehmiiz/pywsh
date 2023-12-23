60e8d6c01989f40bca25e4229fe97082e1a72812
Emil Larsson <43292173+ehmiiz@users.noreply.github.com>
2023-12-23 15:15:39 +0100

    Update ye

---
+++ python_scripts.py
@@ -0,0 +1,17 @@
+# fav nr
+fav = 3
+
+message = f"my fav nr is {fav}"
+print(message)
+
+# 8
+addition = 7+1
+subtract = 9-1
+divide = 8/1
+multiplication = 8*1
+
+print(addition)
+print(subtract)
+print(divide)
+print(multiplication)
+
