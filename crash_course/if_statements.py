56ca4949e85fdb8672357ecb7c79976b2fa368cb
ehmiiz <elarsson17@gmail.com>
2023-12-26 20:55:39 +0100

    y

---
+++ crash_course/if_statements.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'tesla', 'subaru', 'volvo']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+fav_number = 42
+print(fav_number == 41)
